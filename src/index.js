2632d9258e6250262ffaadbaeda119a1ed2c6782
beitaz <beitaz@icloud.com>
2018-02-19 16:38:32 +0800

    修改 index.js 页面使用 react

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-// eslint-disable-next-line
-console.log('Hello');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    <span>Hello</span>
+  </div>
+);
+
+ReactDOM.reander(
+  <App/>,
+  document.getElementById('app'));
