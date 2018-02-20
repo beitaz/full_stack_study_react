ffbf7dbd205d8b8e65297a5c0d94674427d8526b
beitaz <beitaz@icloud.com>
2018-02-20 14:19:03 +0800

    添加 copy-webpack-plugin 并添加 mock 数据

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 // const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: [
@@ -44,6 +45,10 @@ module.exports = {
       favicon: 'public/favicon.png'
     }),
     // new webpack.HotModuleReplacementPlugin(),
+    new CopyWebpackPlugin([{
+      from: 'api',
+      to: 'api'
+    }])
   ],
   devServer: {
     host: '0.0.0.0',
